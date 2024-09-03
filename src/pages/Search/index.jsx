c5ec88fb34f97792c6b8480cd46a3b0caf004021
DavidChung0305 <qwert9789789469@gmail.com>
2024-09-03 20:07:07 +0800

    feat:searchCard

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -12,19 +12,8 @@ const Search = () =>{
 
   console.log(searchVideos)
  return (<>
-  <div  className="mt-3 ml-[200px] flex w-[1069px] h-[290px] hover:bg-slate-500 hover:scale-105 duration-200 rounded-lg">
-      <img src={beigow} className="w-[440px] h-[280px] ml-2 my-auto rounded-lg object-cover" />
-      <div className="w-[650px] h-full ml-3">
-          <h1 className="mt-3 text-[19px] ">你各位。</h1>
-          <p className="text-[13px]  mt-2 text-slate-400">talk街舞  ，  觀看次數：2次 ， 兩個月前</p>
-          <div  className="flex justify-start mt-3 ml-2 w-full items-center p-2 rounded-md " >
-            <img src={snow} className="h-[40px] rounded-full" />
-            <span className="ml-3" >ps</span>
-          </div>
-      </div>
-  </div>
   {searchVideos.map( item => (
-    <SearchCard title={item.title} image={item.thumbnails?.maxres} channelTitle={item.channelTitle} 
+    <SearchCard title={item.title} image={item.thumbnails?.maxres?.url} channelTitle={item.channelTitle} 
     channelImage={item.channelImage} onClick={() => navigate(`/video/${item.videoId}`)}
     />
   ))}
