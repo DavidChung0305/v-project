d59e39989ae8c612fe07b904d230b61a1ff92f79
DavidChung0305 <qwert9789789469@gmail.com>
2025-02-03 15:08:17 +0800

    feat:

--- src/components/VideoChannelCard.jsx
+++ src/components/VideoChannelCard.jsx
@@ -1,13 +1,20 @@
-const VideoChannelCard = ({ image, title, sNumber, onClick}) =>{
+import SubscribeButton from './SubscribeButton';
+import clsx from 'clsx'
+import { useState, useEffect } from 'react'
+import { useVideoStore } from "@/store/video.js"
+
+const VideoChannelCard = ({ image, title, sNumber, onClick, id}) =>{
   return(
-  <div className="flex w-[450px] mt-3 cursor-pointer" onClick={onClick}>
+  <div className='flex'>
+  <div className="flex w-[300px] mt-3 cursor-pointer" onClick={onClick}>
     <img className=" w-[80px] h-[80px] rounded-full" src={image}/>
     <div className="pl-[40px] w-[300px]">
       <p className="text-[25px]">{title}</p>
       <p className="text-[15px] text-gray-500">{sNumber}</p>
     </div>
-    <i className="fa-regular fa-bell w-[120px] h-[32px] text-xl pl-[8px] py-0.5   mt-[30px] border border-solid  border-white rounded-lg  hover:bg-sidebarButtonColor hover:scale-105 duration-200"></i>
-</div>
+  </div>
+  <SubscribeButton id={id} className='h-[20px]' />
+</div> 
   )
 }
 
